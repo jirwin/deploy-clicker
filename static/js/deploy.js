3f6349fbc216cd9c4b1709c2dd7d5249ebfa28d9
Justin Gallardo <justin.gallardo@rackspace.com>
2013-10-11 13:42:23 -0700

    Add CSRF support.

--- static/js/deploy.js
+++ static/js/deploy.js
@@ -2,9 +2,15 @@ $(function() {
   $(document).on('click', '#deploy', function(e) {
     var csrf = $('#csrf').val();
 
-    $.post('/deploy', {'_csrf': csrf})
-      .done(function(data) {
+    $.ajax({
+      type: 'POST',
+      beforeSend: function(req) {
+        req.setRequestHeader('X-XSRF-TOKEN', csrf);
+      },
+      url: '/deploy',
+      success: function(data) {
         $('#deploy-count').text(data.toString());
-      })
+      }
+    });
   });
 });
