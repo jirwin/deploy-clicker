72c42b388cfb0ea11a273d9a7e94eb7bccfdd48e
Justin Gallardo <justin.gallardo@rackspace.com>
2013-10-11 11:46:35 -0700

    Final fixes for version 1.

--- static/js/deploy.js
+++ static/js/deploy.js
@@ -1,1 +1,10 @@
+$(function() {
+  $(document).on('click', '#deploy', function(e) {
+    var csrf = $('#csrf').val();
 
+    $.post('/deploy', {'_csrf': csrf})
+      .done(function(data) {
+        $('#deploy-count').text(data.toString());
+      })
+  });
+});
